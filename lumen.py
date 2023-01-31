29333a5122c113c2b8cbcc50198d1778921e6f95
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2023-01-31 12:07:56 +0200

    Update lumen.py

--- lumen.py
+++ lumen.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.write('hello lumen')
+
+link = '[Stechlux] https://drive.google.com/file/d/1DaGjhoJHWVLROnEiBWuwpAxB7et7QpUh/view?usp=share_link'
+st.markdown(link, unsafe_allow_html=True)
