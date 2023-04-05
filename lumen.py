2d64df0a51f719d366c59ce1a9cc4f42d9d45be5
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2023-04-05 13:40:17 +0300

    Update lumen.py

--- lumen.py
+++ lumen.py
@@ -35,7 +35,7 @@ df = pd.DataFrame(
     [
         {"command": "st.selectbox", "rating": 4, "is_widget": True},
         {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
+        {"command": "st.time_input", "rating": "https://trevos.eu", "is_widget": True},
     ]
 )
 
