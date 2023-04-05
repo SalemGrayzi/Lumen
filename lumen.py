25faa98c53311d0c6c667971a4ba7368a7521494
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2023-04-05 13:36:46 +0300

    Update lumen.py

--- lumen.py
+++ lumen.py
@@ -39,4 +39,7 @@ df = pd.DataFrame(
     ]
 )
 
-st.dataframe(df, use_container_width=True)
+edited_df = st.experimental_data_editor(df, num_rows="dynamic")
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
