94f4dabcf98a778da130506d471cacf4f6c3eb10
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2023-04-05 17:16:56 +0300

    Update lumen.py

--- lumen.py
+++ lumen.py
@@ -13,7 +13,7 @@ with placeholder.form("login"):
     st.markdown("#### For Lumen Employees Only")
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
-    submit = st.form_submit_button("Login")
+    submit = st.form_submit_button("Login123")
 
 if submit and email == actual_email and password == actual_password:
     # If the form is submitted and the email and password are correct,
