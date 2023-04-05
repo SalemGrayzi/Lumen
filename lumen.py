2c8cc79afd7d5b7c6ec8316edbab35562948d60b
SalemGrayzi <99489268+SalemGrayzi@users.noreply.github.com>
2023-04-05 14:45:51 +0300

    Update lumen.py

--- lumen.py
+++ lumen.py
@@ -5,9 +5,9 @@ import pandas as pd
 st.title("Light Fixtures Dashboard")
 
 # create an expander for the first light fixture
-with st.expander('Industrial Lighting'): ##with st.beta_expander('Industrial Lighting'):
+with st.beta_expander('Industrial Lighting'):
     # add the selectbox for the brands
-    st.image('https://www.ledvance.asia/00_Free_To_Use/55282/image-thumb__55282__category_listing/asset-55282_707950_LEDVANCE_Damp_Proof_LED_@2x.webp', use_column_width=True)
+    st.image('https://nvcuk.net/_panel/public/site-panel/products-subcategories/12/id12-image_1630478538.png', use_column_width=True)
     if st.button('Show Brands'):
 
         df = pd.DataFrame(
@@ -23,8 +23,10 @@ with st.expander('Industrial Lighting'): ##with st.beta_expander('Industrial Lig
             {"Brand": "Trilux", "Link": "https://www.trilux.com/en/"},
             {"Brand": "Intra Lighting", "Link": 'https://www.intra-lighting.com'},
             {"Brand": "Delta Light", "Link": 'https://deltalight.com/en/products?perPage=12&filters[collection]=14b97380-4974-11ed-a67a-00505693bd3f&filters[location]=7a8f0b24-2f76-11ed-9132-00505693b57f&sorting=newest'},
-            {"Brand": "Tryka", "Link": "https://tryka.com/products"},
+            {"Brand": "Stahl", "Link": "https://r-stahl.com/en/global/products/"},
+            {"Brand": "Cooper Lighting", "Link": "https://www.cooperlighting.com/global/search?#tab=products"},
+            {"Brand": "Ikizler", "Link": "https://www.ikizlerlighting.com"},
             ]
         )
 
-        st.dataframe(df)##edited_df = st.experimental_data_editor(df, num_rows="dynamic")
+        edited_df = st.experimental_data_editor(df, num_rows="dynamic")
